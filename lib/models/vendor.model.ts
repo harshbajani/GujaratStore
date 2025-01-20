b10895b54ac044e6fe7b343121c01e1580eee014
harshbajani <harshbajani7@gmail.com>
2025-01-20 12:24:42 +0530

    feat: Add store profile functionality with validation and schema updates

--- lib/models/vendor.model.ts
+++ lib/models/vendor.model.ts
@@ -1,21 +1,19 @@
 import mongoose from "mongoose";
 
 const storeAddressSchema = new mongoose.Schema({
-  name: { type: String, required: true, minlength: 2 },
-  contact: { type: String, required: true, minlength: 10 },
-  type: { type: String },
   address_line_1: { type: String, required: true },
   address_line_2: { type: String, required: true },
   locality: { type: String, required: true },
   pincode: { type: String, required: true, minlength: 6 },
   state: { type: String, required: true },
   landmark: { type: String, optional: true },
-  alternativeContact: { type: String, optional: true },
 });
 
 const store = new mongoose.Schema({
-  name: { type: String, required: true, minlength: 2 },
+  storeName: { type: String, required: true, minlength: 2 },
+  contact: { type: String, required: true, minlength: 10 },
   addresses: { type: storeAddressSchema, required: true },
+  alternativeContact: { type: String, optional: true },
 });
 
 const vendorSchema = new mongoose.Schema({
