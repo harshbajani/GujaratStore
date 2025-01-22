6fe6544adca412d372676479a8522f4e59f9aa2b
harshbajani <harshbajani7@gmail.com>
2025-01-22 15:54:43 +0530

    fix: Enhance sidebar with expandable categories and add new category pages

---
+++ app/vendor/(root)/category/secondaryCategory/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { withVendorProtection } from "@/app/vendor/HOC";
+import { LayoutPanelLeft } from "lucide-react";
+import React from "react";
+
+const SecondaryCategoryPage = () => {
+  return (
+    <div className="p-2 ">
+      <div className="flex items-center gap-2">
+        <LayoutPanelLeft className="text-brand" size={30} />
+        <h1 className="h1">Secondary Category</h1>
+      </div>
+      <div className="p-2 bg-white border rounded-md min-h-screen">
+        dashboard componenets and functions to calculate and display data
+      </div>
+    </div>
+  );
+};
+
+export default withVendorProtection(SecondaryCategoryPage);
