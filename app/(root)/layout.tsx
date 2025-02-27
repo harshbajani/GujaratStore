0b85057c9293d6b4002c1163e397fc9641e9b50f
harshbajani <harshbajani7@gmail.com>
2025-02-27 13:00:41 +0530

    refactor: improve smooth scrolling functionality and viewport handling

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,7 +5,7 @@ import Header from "@/components/Header";
 import { Toaster } from "@/components/ui/toaster";
 import { SessionProvider } from "next-auth/react";
 import { ParallaxProvider } from "react-scroll-parallax";
-import { SmoothScrollProvider } from "@/components/SmoothScrollProvider";
+// import { SmoothScrollProvider } from "@/components/SmoothScrollProvider";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -13,16 +13,16 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <body className="flex min-h-screen flex-col overflow-x-hidden">
         <SessionProvider basePath="/api/auth">
           <ParallaxProvider>
-            <SmoothScrollProvider>
-              <div className="smooth-scroll-container">
-                <Header />
-                <main className="pt-[16px] md:pt-[52px] flex-grow">
-                  {children}
-                </main>
-                <Footer />
-              </div>
-              <Toaster />
-            </SmoothScrollProvider>
+            {/* <SmoothScrollProvider> */}
+            <div className="smooth-scroll-container">
+              <Header />
+              <main className="pt-[16px] md:pt-[52px] flex-grow">
+                {children}
+              </main>
+              <Footer />
+            </div>
+            <Toaster />
+            {/* </SmoothScrollProvider> */}
           </ParallaxProvider>
         </SessionProvider>
       </body>
