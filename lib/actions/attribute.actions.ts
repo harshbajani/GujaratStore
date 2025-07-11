487ea37aa5ac4ce0481ac231ccc5d800e45d9dc7
harshbajani <harshbajani7@gmail.com>
2025-07-11 15:43:13 +0530

    feat: integrate database connection in attribute actions
    - Added `connectToDB` function call in `getAllAttributes`, `getAttributeById`, `updateAttribute`, and `deleteAttribute` functions to ensure database connectivity before performing operations.
    - This change enhances the reliability of attribute-related actions by establishing a connection to the database as needed.

--- lib/actions/attribute.actions.ts
+++ lib/actions/attribute.actions.ts
@@ -2,6 +2,7 @@
 
 import { revalidatePath } from "next/cache";
 import { AttributeService } from "@/services/attribute.service";
+import { connectToDB } from "../mongodb";
 
 export async function createAttribute(name: string, isActive: boolean) {
   const result = await AttributeService.createAttribute(name, isActive);
@@ -10,20 +11,24 @@ export async function createAttribute(name: string, isActive: boolean) {
 }
 
 export async function getAllAttributes() {
+  await connectToDB();
   return await AttributeService.getAllAttributes();
 }
 
 export async function getAttributeById(id: string) {
+  await connectToDB();
   return await AttributeService.getAttributeById(id);
 }
 
 export async function updateAttribute(id: string, data: AttributeFormData) {
+  await connectToDB();
   const result = await AttributeService.updateAttribute(id, data);
   if (result.success) revalidatePath("/vendor/attribute");
   return result;
 }
 
 export async function deleteAttribute(id: string) {
+  await connectToDB();
   const result = await AttributeService.deleteAttribute(id);
   if (result.success) revalidatePath("/vendor/attribute");
   return result;
